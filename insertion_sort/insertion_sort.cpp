fbbb9cb5f5e65a2e5d47aa87abdb1b0a9bfadd6b
Rodrigo <rodrigo8a13@gmail.com>
2026-09-23 10:37:54 -0600

    feat: Mostrar Resultados

--- insertion_sort/insertion_sort.cpp
+++ insertion_sort/insertion_sort.cpp
@@ -13,19 +13,45 @@ void mostrarArreglo(int numeros[], int n)
 
 void insertionSort(int numeros[], int n)
 {
+    int comparaciones = 0;
+    int desplazamientos = 0;
+    int inserciones = 0;
+
     for (int i = 1; i < n; i++)
     {
         int clave = numeros[i];
         int j = i - 1;
 
-        while (j >= 0 && numeros[j] > clave)
+        cout << "\nInsertando: " << clave << endl;
+
+        while (j >= 0)
         {
-            numeros[j + 1] = numeros[j];
-            j--;
+            comparaciones++;
+
+            if (numeros[j] > clave)
+            {
+                numeros[j + 1] = numeros[j];
+                desplazamientos++;
+                j--;
+            }
+            else
+            {
+                break;
+            }
         }
 
         numeros[j + 1] = clave;
+        inserciones++;
+
+        mostrarArreglo(numeros, n);
     }
+
+    cout << "\n========================================" << endl;
+    cout << "       ESTADISTICAS" << endl;
+    cout << "========================================" << endl;
+    cout << "Comparaciones: " << comparaciones << endl;
+    cout << "Desplazamientos: " << desplazamientos << endl;
+    cout << "Inserciones: " << inserciones << endl;
 }
 
 int main()
@@ -37,9 +63,6 @@ int main()
     mostrarArreglo(numeros, n);
 
     insertionSort(numeros, n);
-
-    cout << "Arreglo ordenado:" << endl;
-    mostrarArreglo(numeros, n);
 
     return 0;
 }
